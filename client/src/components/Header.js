14f7db1810d170609223cf58438b29c04fcdb082
npezzotti <npezzotti@hotmail.com>
2019-10-22 19:56:30 -0400

    created header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-function Header() {
+const Header = () => {
   return (
-    <div>
-     
+    <div className="header">
+      <Link className="home-link" to="/">
+        <h1 className="title">Outfit Creator</h1>
+      </Link>
+      <nav className="nav-bar">
+        <Link className="nav-link" to="/animals">
+          <button className="link-button">Clothing</button>
+        </Link>
+        <Link className="nav-link" to="/random">
+          <button className="link-button">Outfits</button>
+        </Link>
+        <Link className="nav-link" to="/status">
+          <button className="link-button">Create Clothing</button>
+        </Link>
+        <Link className="nav-link" to="/status">
+          <button className="link-button">Create Outfit</button>
+        </Link>
+      </nav>
     </div>
   );
 }
