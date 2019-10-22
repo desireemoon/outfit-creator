0bea7c5c86fc115a4d4e0a954abcc64a07598b02
desireemoon <desiree.dewysocki@gmail.com>
2019-10-22 11:50:27 -0400

    reworking backend

--- models.js
+++ models.js
@@ -6,146 +6,208 @@ const sequelize = new Sequelize(
     }
 )
 
-export class Article extends Model {}
 
-Article.init(
-    {   name : {
-        type: DataTypes.STRING,
-        unique: true
-    },
-        creator : DataTypes.STRING,
-        type : DataTypes.STRING,
-        color : DataTypes.STRING,
-        fabric: DataTypes.STRING,
-        brand: DataTypes.STRING,
-        url: DataTypes.TEXT,
+
+export class Hat extends Model {}
+
+Hat.init(
+    {   
+        name : {
+            type: DataTypes.STRING,
+            unique: true
+        },
         imgUrl: DataTypes.TEXT
+    },
+    {   
+        freezeTableName: true,     // stops sequelize from doing its automatic pluralization
+        tableName: "Hat", // the custom name to use for the Table
+        sequelize   // this is where we pass the database connection to the model
     }
-    ,  // <- Don't miss the comma between the objects
-    {   freezeTableName: true,     // stops sequelize from doing its automatic pluralization
-        tableName: "Article", // the custom name to use for the Table
+
+
+)
+
+export class Shirt extends Model {}
+
+Shirt.init(
+    {   
+        name : {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        imgUrl: DataTypes.TEXT
+    },
+    {   
+        freezeTableName: true,     // stops sequelize from doing its automatic pluralization
+        tableName: "Shirt", // the custom name to use for the Table
         sequelize   // this is where we pass the database connection to the model
     }
 )
 
 export class Outfit extends Model {}
 
-Outfit.init(
-    {   name : DataTypes.STRING,
-        hat: {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        scarf: {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        buttonUp : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        shirt : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        sweater : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        pants : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        dress : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        skirt : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        shoes : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          },
-        jacket : {
-            type: Sequelize.STRING,
-            references: {
-              // This is a reference to another model
-              model: Article,
-              // This is the column name of the referenced model
-              key: 'name',
-              // This declares when to check the foreign key constraint. PostgreSQL only.
-              deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-            }
-          }
-    }
-    ,  
-    {   freezeTableName: true,     
-        tableName: "Outfit", 
-        sequelize   
+Outfit.init (
+    {
+        name : {
+        type: DataTypes.STRING,
+        unique: true
+        },
+        hat_id: DataTypes.INTEGER,
+        Shirt_id: DataTypes.INTEGER
+    },
+    {   
+        freezeTableName: true,     // stops sequelize from doing its automatic pluralization
+        tableName: "Outfit", // the custom name to use for the Table
+        sequelize   // this is where we pass the database connection to the model
     }
 )
+
+Hat.hasMany(Outfit)
+Outfit.belongsTo(Hat)
+
+Shirt.hasMany(Outfit)
+Outfit.belongsTo(Shirt)
+
+// export class Article extends Model {}
+
+// Article.init(
+//     {   name : {
+//         type: DataTypes.STRING,
+//         unique: true
+//     },
+//         creator : DataTypes.STRING,
+//         type : DataTypes.STRING,
+//         color : DataTypes.STRING,
+//         fabric: DataTypes.STRING,
+//         brand: DataTypes.STRING,
+//         url: DataTypes.TEXT,
+//         imgUrl: DataTypes.TEXT
+//     }
+//     ,  // <- Don't miss the comma between the objects
+//     {   freezeTableName: true,     // stops sequelize from doing its automatic pluralization
+//         tableName: "Article", // the custom name to use for the Table
+//         sequelize   // this is where we pass the database connection to the model
+//     }
+// )
+
+// export class Outfit extends Model {}
+
+// Outfit.init(
+//     {   name : DataTypes.STRING,
+//         hat: {
+//             type: DataTypes.STRING,
+//             references: {
+//               // This is a reference to another model
+//               model: Article,
+//               // This is the column name of the referenced model
+//               key: 'name',
+//               // This declares when to check the foreign key constraint. PostgreSQL only.
+//               deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+//             }
+//           },
+        // scarf: {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // buttonUp : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // shirt : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // sweater : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // pants : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // dress : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // skirt : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // shoes : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   },
+        // jacket : {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       // This is a reference to another model
+        //       model: Article,
+        //       // This is the column name of the referenced model
+        //       key: 'name',
+        //       // This declares when to check the foreign key constraint. PostgreSQL only.
+        //       deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        //     }
+        //   }
+//     }
+//     ,  
+//     {   freezeTableName: true,     
+//         tableName: "Outfit", 
+//         sequelize   
+//     }
+// )
