dcaf2c4102e9f2a6f45b62c2896a742dfdc8b8db
npezzotti <npezzotti@hotmail.com>
2019-10-22 15:44:39 -0400

    built footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Footer = () => {
     return(
-        <div>
-            
+        <div className="footer-container">
+            <div class="social-title">
+                <h2 className="footer-header">Connect with the programmers:</h2>
+            </div>
+            <div className="links">
+                {/* UPDATE ICONS LATER */}
+                <div class="social"><a href="https://git.generalassemb.ly/desireemoon/outfit-creator"><FontAwesomeIcon icon={['fab', 'linkedin']} size="3x" className="font-icon" /></a></div>
+            </div>
         </div>
     )
 }
