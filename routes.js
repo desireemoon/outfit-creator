e8cad65e183587e0aee617e27b817144814c6227
desireemoon <desiree.dewysocki@gmail.com>
2019-10-22 12:41:09 -0400

    adding routes for hat and shirt

--- routes.js
+++ routes.js
@@ -1,14 +1,26 @@
 import express from 'express' // <- this import is special for this file
 import { Router } from 'express'
-import { Article, Outfit } from './models'
+import { Hat, Shirt, Outfit } from './models'
 
-const getAllArticles = async (
+const getAllHats = async (
     /** @type {express.Request} */  req,    // <-the type annotation for the request argument
     /** @type {express.Response}*/  res     // <-the type annotation for the response argument
     ) => {
     try {
-        const articles = await Article.findAll()
-        return res.status(200).json({articles})
+        const hats = await Hat.findAll()
+        return res.status(200).json({hats})
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const getAllShirts = async (
+    /** @type {express.Request} */  req,    // <-the type annotation for the request argument
+    /** @type {express.Response}*/  res     // <-the type annotation for the response argument
+    ) => {
+    try {
+        const hats = await Hat.findAll()
+        return res.status(200).json({hats})
     } catch (error) {
         return res.status(500).send(error.message)
     }
