3899dfcc316893193aa402efee89056c77e01a18
desireemoon <desiree.dewysocki@gmail.com>
2019-10-23 17:02:47 -0400

    cleaned up backend code

--- resetDb.js
+++ resetDb.js
@@ -1,10 +1,8 @@
-import { Hat, Shirt, Article, Outfit, OutfitArticle } from './models'
+import { Article, Outfit, OutfitArticle } from './models'
 
 
 const main = async () => {
-    // await Hat.sync({force:true})
     await Article.sync({force:true})
-    // await Shirt.sync({force:true})
     await Outfit.sync({force:true})
     await OutfitArticle.sync({force:true})
 
--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 import express from 'express' // <- this import is special for this file
 import { Router } from 'express'
-import { Hat, Shirt, Article, Outfit, OutfitArticle } from './models'
+import { Article, Outfit, OutfitArticle } from './models'
 
 const getAllArticles = async (
         /** @type {express.Request} */  req,    // <-the type annotation for the request argument
@@ -163,115 +163,82 @@ const updateArticle = async (req,res) => {
         }
     };
     
-    // const updateOutfit = async (req,res) => {
-    //     try {
-    //         const updated = await Outfit.update(req.body, {
-    //             where: { id: req.params.id }
-    //         });
-    //         console.log(updated)
-    //         if (updated) {
-    //             const updatedOutfit = await Outfit.findOne({ where: { id: req.params.id } });
-    //             req.body.articles.forEach( async article => {
-    //                 const newArticle = await Article.findByPk(article.id)
-    //                if(!newArticle) {
-    //                    return res.status(400)
-    //                }
-    //                const oa = {
-    //                    outfit_id: req.params.id,
-    //                    article_id: article.id
-    //                }
-    //                 console.log(oa)
-    //                 const outfitArticle = await OutfitArticle.update(oa, {where: { outfit_id: req.params.id }})
-    //            });
-    //            return res.status(201).json(
-    //                 {
-    //                     outfit:{
-    //                     updatedOutfit
-    //                     }
-    //                 }
-    //             )
-    //         }
-            
-    //     } catch(error) {
-    //         return res.status(500).send(error)
-    //     }
-    // };
-    const updateOutfit = async (req,res) => {
-            try {
-                const updated = await Outfit.update(req.body, {
-                    where: { id: req.params.id }
-                });
-                if (updated) {
-                    const updatedOutfit = await Outfit.findOne({ where: { id: req.params.id } });
-                    return res.status(200).json({ updatedOutfit });
-                }
-                throw new Error('Item not found');
-            } catch (error) {
-                return res.status(500).send(error.message);
+const updateOutfit = async (req,res) => {
+        try {
+            const updated = await Outfit.update(req.body, {
+                where: { id: req.params.id }
+            });
+            if (updated) {
+                const updatedOutfit = await Outfit.findOne({ where: { id: req.params.id } });
+                return res.status(200).json({ updatedOutfit });
             }
-        };
-
-    const addArticleToOutfit = async (req, res) => {
-        try {
-            const outfitWithArticles = await Outfit.findOne({
-                where: { id: req.params.outfit_id },
-                include: [Article]
-            });
-            if (outfitWithArticles.Articles.find( art => art.id===req.params.id)) {
-                 res.status(400)
-            } 
-            const article = await Article.findByPk(req.params.id)
-            await outfitWithArticles.addArticle(article)
-            // grab Article with id from req.perams.id 
-            //outfit.addArticle(_)
-            //return new outfit
-            const newOutfit = await Outfit.findOne({
-                where: { id: req.params.outfit_id },
-                include: [Article]
-            });
-            res.json(newOutfit);
-        } catch (e) {
-            res.status(400).json(e.message);
+            throw new Error('Item not found');
+        } catch (error) {
+            return res.status(500).send(error.message);
         }
-    }
+    };
 
-    const deleteArticleFromOutfit = async (req, res) => {
-        try {
-            const outfitWithArticles = await Outfit.findOne({
-                where: { id: req.params.outfit_id },
-                include: [Article]
-            });
-            if (!outfitWithArticles.Articles.find( art => art.id===req.params.id)) {
-                res.status(400).send('Article not in outfit!');
-            } 
-            const article = await Article.findByPk(req.params.id)
-            await outfitWithArticles.removeArticle(article)
-            // grab Article with id from req.perams.id 
-            //outfit.addArticle(_)
-            //return new outfit
-            const newOutfit = await Outfit.findOne({
-                where: { id: req.params.outfit_id },
-                include: [Article]
-            });
-            res.json(newOutfit);
-        } catch (e) {
-            res.status(400).json(e.message);
-        }
-    }
-    
-    const deleteArticle = async (req, res) => {
+const addArticleToOutfit = async (req, res) => {
     try {
-        const id = req.params.id
-        const deleted = await Article.destroy({
-            where: {id:id}
-        })
-        if (deleted) {
-            return res.status(200).send('Article deleted')
-        }
-        throw new Error('Article not found')
-    } catch(error) {
-        res.status(500).send(error.message)
+        const outfitWithArticles = await Outfit.findOne({
+            where: { id: req.params.outfit_id },
+            include: [Article]
+        });
+        if (outfitWithArticles.Articles.find( art => art.id===req.params.id)) {
+                res.status(400)
+        } 
+        const article = await Article.findByPk(req.params.id)
+        await outfitWithArticles.addArticle(article)
+        // grab Article with id from req.perams.id 
+        //outfit.addArticle(_)
+        //return new outfit
+        const newOutfit = await Outfit.findOne({
+            where: { id: req.params.outfit_id },
+            include: [Article]
+        });
+        res.json(newOutfit);
+    } catch (e) {
+        res.status(400).json(e.message);
     }
+}
+
+const deleteArticleFromOutfit = async (req, res) => {
+    try {
+        const outfitWithArticles = await Outfit.findOne({
+            where: { id: req.params.outfit_id },
+            include: [Article]
+        });
+        if (!outfitWithArticles.Articles.find( art => art.id===req.params.id)) {
+            res.status(400).send('Article not in outfit!');
+        } 
+        const article = await Article.findByPk(req.params.id)
+        await outfitWithArticles.removeArticle(article)
+        // grab Article with id from req.perams.id 
+        //outfit.addArticle(_)
+        //return new outfit
+        const newOutfit = await Outfit.findOne({
+            where: { id: req.params.outfit_id },
+            include: [Article]
+        });
+        res.json(newOutfit);
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
+}
+
+const deleteArticle = async (req, res) => {
+try {
+    const id = req.params.id
+    const deleted = await Article.destroy({
+        where: {id:id}
+    })
+    if (deleted) {
+        return res.status(200).send('Article deleted')
+    }
+    throw new Error('Article not found')
+} catch(error) {
+    res.status(500).send(error.message)
+}
 }
 
 const deleteOutfit = async (req, res) => {
@@ -312,296 +279,6 @@ closetRouter.put("/outfits/:outfit_id/articles/remove/:id", deleteArticleFromOut
 closetRouter.delete("/articles/:id", deleteArticle)
 closetRouter.delete("/outfits/:id", deleteOutfit)
 
-// const getAllHats = async (
-//     /** @type {express.Request} */  req,    // <-the type annotation for the request argument
-//     /** @type {express.Response}*/  res     // <-the type annotation for the response argument
-//     ) => {
-//     try {
-//         const hats = await Hat.findAll()
-//         return res.status(200).json({hats})
-//     } catch (error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
 
-// const getAllShirts = async (
-//     /** @type {express.Request} */  req,    // <-the type annotation for the request argument
-//     /** @type {express.Response}*/  res     // <-the type annotation for the response argument
-//     ) => {
-//     try {
-//         const hats = await Hat.findAll()
-//         return res.status(200).json({hats})
-//     } catch (error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const getAllOutfits = async (
-//     /** @type {express.Request} */  req,    // <-the type annotation for the request argument
-//     /** @type {express.Response}*/  res     // <-the type annotation for the response argument
-//     ) => {
-//     try {
-//         const outfits = await Outfit.findAll(
-//             {
-//             include: [
-//                 {
-//                 model: Hat
-//             },
-//             {
-//                 model: Shirt
-//             },
-//         ]
-//          }
-//     )
-//         return res.status(200).json({outfits})
-//     } catch (error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const getHatById = async (req,res) => {
-//     try {
-//         const id = req.params.id;
-//         const hat = await Hat.findAll({
-//             where: {id: id}
-//         })
-//         if (hat) {
-//             return res.status(200).json({hat})
-//         }
-//         return res.status(404).send('Hat with the specified ID does not exists');
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const getShirtById = async (req,res) => {
-//     try {
-//         const id = req.params.id;
-//         const shirt = await Shirt.findAll({
-//             where: {id: id}
-//         })
-//         if (shirt) {
-//             return res.status(200).json({shirt})
-//         }
-//         return res.status(404).send('Shirt with the specified ID does not exists');
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const getOutfitById = async (req,res) => {
-//     try {
-//         const id = req.params.id;
-//         console.log(id);
-        
-//         const outfit = await Outfit.findOne(
-//             {
-//                 where: {id: parseInt(id)},
-//                 include: [
-//                     {
-//                         model: Hat
-//                     },
-//                     {
-//                         model: Shirt
-//                     },
-//                 ]
-//             }
-//     )
-//         if (outfit) {
-//             return res.status(200).json({outfit})
-//         }
-//         return res.status(404).send('Outfit with the specified ID does not exists');
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const getHatByName = async (req,res) => {
-//     try {
-//         const name = req.params.name;
-//         const hat = await Hat.findAll({
-//             where: {name: name}
-//         })
-//         if (hat) {
-//             return res.status(200).json({hat})
-//         }
-//         return res.status(404).send('Hat with the specified name does not exists');
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const getShirtByName = async (req,res) => {
-//     try {
-//         const name = req.params.name;
-//         const shirt = await Shirt.findAll({
-//             where: {name: name}
-//         })
-//         if (shirt) {
-//             return res.status(200).json({shirt})
-//         }
-//         return res.status(404).send('Shirt with the specified name does not exists');
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const getOutfitByName = async (req,res) => {
-//     try {
-//         const name = req.params.name;
-//         const outfit = await Outfit.findAll(
-//             {
-//                 where: {name: name},
-//                 include: [
-//                     {
-//                         model: Hat
-//                     },
-//                     {
-//                         model: Shirt
-//                     },
-//                 ]
-//             }
-
-//     )
-//         if (outfit) {
-//             return res.status(200).json({outfit})
-//         }
-//         return res.status(404).send('Outfit with the specified ID does not exists');
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const createHat = async (req, res) => {
-//     try {
-//         const hat = await Hat.create(req.body)
-//         return res.status(201).json({new:{
-//             hat
-//         }})
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const createShirt = async (req, res) => {
-//     try {
-//         const shirt = await Shirt.create(req.body)
-//         return res.status(201).json({new:{
-//             shirt
-//         }})
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const createOutfit = async (req, res) => {
-//     try {
-//         const outfit = await Outfit.create(req.body)
-//         return res.status(201).json({new:{
-//             outfit
-//         }})
-//     } catch(error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
-
-// const updateHat = async (req,res) => {
-//     try {
-//         const updated = await Hat.update(req.body, {
-//             where: { id: req.params.id }
-//         });
-//         if (updated) {
-//             const updatedHat = await Hat.findOne({ where: { id: req.params.id } });
-//             return res.status(200).json({ hat: updatedHat });
-//         }
-//         throw new Error('Item not found');
-//     } catch (error) {
-//         return res.status(500).send(error.message);
-//     }
-// };
-
-// const updateShirt = async (req,res) => {
-//     try {
-//         const updated = await Shirt.update(req.body, {
-//             where: { id: req.params.id }
-//         });
-//         if (updated) {
-//             const updatedShirt = await Shirt.findOne({ where: { id: req.params.id } });
-//             return res.status(200).json({ shirt: updatedShirt });
-//         }
-//         throw new Error('Item not found');
-//     } catch (error) {
-//         return res.status(500).send(error.message);
-//     }
-// };
-
-
-// const updateOutfit = async (req,res) => {
-//     try {
-//         const updated = await Outfit.update(req.body, {
-//             where: { id: req.params.id }
-//         });
-//         if (updated) {
-//             const updatedOutfit = await Outfit.findOne({ where: { id: req.params.id } });
-//             return res.status(200).json({ article: updatedOutfit });
-//         }
-//         throw new Error('Item not found');
-//     } catch (error) {
-//         return res.status(500).send(error.message);
-//     }
-// };
-
-// const deleteArticle = async (req, res) => {
-//     try {
-//         const id = req.params.id
-//         const deleted = await Article.destroy({
-//             where: {id:id}
-//         })
-//         if (deleted) {
-//             return res.status(200).send('Article deleted')
-//         }
-//         throw new Error('Article not found')
-//     } catch(error) {
-//         res.status(500).send(error.message)
-//     }
-// }
-
-// const deleteOutfit = async (req, res) => {
-//     try {
-//         const id = req.params.id
-//         const deleted = await Outfit.destroy({
-//             where: {id:id}
-//         })
-//         if (deleted) {
-//             return res.status(200).send('Outfit deleted')
-//         }
-//         throw new Error('Outfit not found')
-//     } catch(error) {
-//         res.status(500).send(error.message)
-//     }
-// }
-
-// export const closetRouter = Router()
-
-// closetRouter.get("/hats", getAllHats)
-// closetRouter.get("/shirts", getAllShirts)
-// closetRouter.get("/outfits", getAllOutfits)
-
-// closetRouter.get("/hats/id/:id", getHatById)
-// closetRouter.get("/shirts/id/:id", getShirtById)
-// closetRouter.get("/outfits/id/:id", getOutfitById)
-
-// closetRouter.get("/hats/name/:name", getHatByName)
-// closetRouter.get("/shirts/name/:name", getShirtByName)
-// closetRouter.get("/outfits/name/:name", getOutfitByName)
-
-// closetRouter.post("/hats", createHat)
-// closetRouter.post("/shirts", createShirt)
-// closetRouter.post("/outfits", createOutfit)
-
-// closetRouter.put("/articles/id/:id", updateArticle)
-// closetRouter.put("/outfits/id/:id", updateOutfit)
-// closetRouter.delete("/articles/id/:id", deleteArticle)
-// closetRouter.delete("/outfits/id/:id", deleteOutfit)
 
 
