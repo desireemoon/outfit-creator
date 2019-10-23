0eacce38f898d97799ddb6470267beb3562a0bef
npezzotti <npezzotti@hotmail.com>
2019-10-23 10:37:31 -0400

    server running after merge

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import axios from "axios"
-import Item from "./ItemDetails"
+import ItemDetails from "./ItemDetails"
 
 class ItemList extends Component {
   constructor(props) {
