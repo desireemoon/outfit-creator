94f8e7455f2c9376714b1e1d6df6fb51d3a9adf6
desireemoon <desiree.dewysocki@gmail.com>
2019-10-24 14:21:08 -0400

    preparing deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
 import { closetRouter } from './routes'
-
+const path = require('path');
 const app = express()
 
 app.use(express.json())
@@ -14,6 +14,11 @@ app.get("/test", (req,res) => {
     return res.header(200).send({greetings: "we're cooking with GAS!!! o(*￣▽￣*)ブ"})
 })
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const PORT = 3636
 
